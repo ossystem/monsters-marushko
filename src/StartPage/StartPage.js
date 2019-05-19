f760569a92d24b0d38b8a18fdeb76c7612902633
Vitalii Marushko <vi.marushko@gmail.com>
2019-05-19 18:42:28 +0300

    Added adaptive design for star page component; changed npm scripts

--- src/StartPage/StartPage.js
+++ src/StartPage/StartPage.js
@@ -7,8 +7,12 @@ class StartPage extends Component {
   render () {
     return (
       <div className="container">
-        <img className="top-logo" src="img/start_logo.png" alt=""/>
+        <img className="top-logo" src="img/logo.png" alt=""/>
+        <div className="top-logo-label">
+          Found your monsters
+        </div>
         <img className="all-monsters" src="img/page_1_monsters.png" alt=""/>
+        <img className="all-monsters-mob" src="img/page_mob_1_monsters.png" alt=""/>
         <ButtonNext
           text="Start"
           toRoute={this.props.idToken ? "/questions/2/1" : "/questions/1/1"}
